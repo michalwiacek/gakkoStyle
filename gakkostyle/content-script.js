1d6596ebf103c6f50c1d4668f0fc64413967626d
Michał Wiącek <6273138+michalwiacek@users.noreply.github.com>
2023-12-13 17:16:13 +0100

    Update content-script.js

--- gakkostyle/content-script.js
+++ gakkostyle/content-script.js
@@ -1,4 +1,3 @@
-// Tworzenie przycisku trybu ciemnego
 const darkModeButton = document.createElement('button');
 darkModeButton.id = 'darkModeToggle';
 darkModeButton.innerText = 'Tryb ciemny';
@@ -7,30 +6,24 @@ darkModeButton.style.top = '10px';
 darkModeButton.style.right = '10px';
 darkModeButton.style.zIndex = '1000';
 
-// Dodawanie przycisku do strony
 document.body.appendChild(darkModeButton);
 
-// Zmienna do śledzenia stanu
 let isDarkMode = false;
 
-// Funkcja do przełączania trybów
 function toggleDarkMode() {
     if (isDarkMode) {
-        // Usuwamy styl trybu ciemnego
         var existingLinks = document.querySelectorAll('link[href="' + chrome.runtime.getURL('dark-mode.css') + '"]');
         existingLinks.forEach(link => link.remove());
-        darkModeButton.innerText = 'Tryb ciemny';
+        darkModeButton.innerText = 'Dark Mode';
         isDarkMode = false;
     } else {
-        // Dodajemy styl trybu ciemnego
         var link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = chrome.runtime.getURL('dark-mode.css');
         document.head.appendChild(link);
-        darkModeButton.innerText = 'Tryb jasny';
+        darkModeButton.innerText = 'Light Mode';
         isDarkMode = true;
     }
 }
 
-// Dodanie nasłuchiwania na kliknięcie
 darkModeButton.addEventListener('click', toggleDarkMode);
