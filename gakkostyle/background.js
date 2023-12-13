e7b5b05389167649380cb88a460d88d1166d927b
Michał Wiącek <6273138+michalwiacek@users.noreply.github.com>
2023-12-13 17:16:28 +0100

    Update background.js

--- gakkostyle/background.js
+++ gakkostyle/background.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action == "activateDarkMode") {
-            console.log("Received message to activate dark mode");
             chrome.tabs.insertCSS(sender.tab.id, { file: "dark-mode.css" });
         }
     }
